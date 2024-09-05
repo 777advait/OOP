faebbe36bc794dcff85e820e14fc66d1e8d32fa1
advait <76993204+777advait@users.noreply.github.com>
2024-09-05 05:18:12 +0000

    add exp 11 prac question 1

---
+++ EXP_11/prac_question_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    protected:
+        int roll_no;
+        char name[100];
+
+    public:
+        void get() {
+            cout << "Enter roll no.: ";
+            cin >> roll_no;
+            cout << "Enter name: ";
+            cin >> name;
+        }
+};
+
+class Marks: protected Student {
+    private:
+        float m1, m2, m3, total, percentage;
+
+    public:
+        void get() {
+            Student::get();
+            cout << "Enter M1 marks: ";
+            cin >> m1;
+            cout << "Enter M2 marks: ";
+            cin >> m2;
+            cout << "Enter M3 marks: ";
+            cin >> m3;
+        }
+
+        void display() {
+            total = m1 + m2 + m3;
+            percentage = total /3;
+
+            cout << "Percentage" << percentage;
+        }
+};
+
+int main() {
+    Marks m;
+
+    m.get();
+    m.display();
+
+    return 0;
+}
